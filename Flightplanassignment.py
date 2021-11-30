29d978011cd427db4beb2823765cfc599d5b287e
oofbro23 <92953634+oofbro23@users.noreply.github.com>
2021-11-30 15:50:40 -0800

    Update Flightplanassignment.py

--- Flightplanassignment.py
+++ Flightplanassignment.py
@@ -2,19 +2,15 @@ from djitellopy import Tello
 
 from time import sleep
 
-
-kp.init()
 tello = Tello()
-tello.connect
-print(tello.get_battery())
+
+tello.connect()
 
 tello.takeoff()
 
-# 32in = 82cm
+tello.move_up(181)
 
-tello.move_up(101)
-
-tello.move(152)
+tello.move_forward(152)
 sleep(1)
 
 tello.rotate_counter_clockwise(90)
@@ -31,14 +27,13 @@ sleep(1)
 
 tello.rotate_clockwise(90)
 
+tello.move_up(30)
+
 tello.move_forward(91)
 sleep(1)
 
-tello.rotate_clockwise(90)
-
-tello.move_up(30)
+tello.rotate_counter_clockwise(90)
 
 tello.move_forward(183)
 
-tello.move_down(183)
-
+tello.land()
